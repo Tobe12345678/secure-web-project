fb5efded6af11c8a56b31703370c886cdae6dacb
Tobe12345678 <ogunedotobechukwu@gmail.com>
2024-12-01 11:13:49 +0000

    Fixing the frontend

--- bankist-backend/server.js
+++ bankist-backend/server.js
@@ -1,23 +1,20 @@
-// Import Express
+// Import express
 const express = require('express');
 const path = require('path');
 const app = express();
 
-// Middleware to parse JSON (optional, depending on your needs)
-app.use(express.json());
-
-// Serve static files from the `Bankist-main` folder
+// Code to serve static files from the `Bankist-main` folder
 app.use(express.static(path.join(__dirname, '../Bankist-main')));
 
-// Define the path to the `index.html` file
-const INDEX_PATH = path.join(__dirname, '..', 'index.html'); // Adjust path for one level up
+// Code to Define the path to the `index.html` file
+const INDEX_PATH = path.join(__dirname, '..', 'index.html');
 
-// Define the root route
+// Code to define the root route
 app.get('/', (req, res) => {
   res.sendFile(INDEX_PATH);
 });
 
-// Start the server
+// Code to start the server
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
